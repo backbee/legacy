937cf2a1ef14f03c5a661e5fa5d260f33c3761ac
Djoudi <djoudi.bensid@lp-digital.fr>
2021-11-09 18:52:41 +0100

    [#664] - Session not deleted after a logout.

--- Rest/Controller/SecurityController.php
+++ Rest/Controller/SecurityController.php
@@ -21,6 +21,10 @@
 
 namespace BackBee\Rest\Controller;
 
+use BackBee\Rest\Controller\Annotations as Rest;
+use BackBee\Security\Token\BBUserToken;
+use BackBee\Security\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
@@ -28,9 +32,6 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Security\Core\Exception\DisabledException;
 use Symfony\Component\Validator\Constraints as Assert;
-
-use BackBee\Rest\Controller\Annotations as Rest;
-use BackBee\Security\Token\BBUserToken;
 
 /**
  * Auth Controller.
@@ -46,26 +47,27 @@ class SecurityController extends AbstractRestController
      * @Rest\RequestParam(name="username", requirements={@Assert\NotBlank})
      * @Rest\RequestParam(name="password", requirements={@Assert\NotBlank})
      */
-    public function authenticateAction(Request $request)
+    public function authenticateAction(Request $request): JsonResponse
     {
         $created = date('Y-m-d H:i:s');
         $token = new BBUserToken();
         $token->setUser($request->request->get('username'));
         $token->setCreated($created);
         $token->setNonce(md5(uniqid('', true)));
-        $encodedPassword = $this
+
+        if ($factory = $this->getApplication()->getSecurityContext()->getEncoderFactory()) {
+            $encodedPassword = $factory
+                ->getEncoder(User::class)
+                ->encodePassword($request->request->get('password'), '');
+        }
+
+        $token->setDigest(md5($token->getNonce() . $created . $encodedPassword));
+
+        $tokenAuthenticated = $this
             ->getApplication()
             ->getSecurityContext()
-            ->getEncoderFactory()
-            ->getEncoder('BackBee\Security\User')
-            ->encodePassword($request->request->get('password'), '')
-        ;
-
-        $token->setDigest(md5($token->getNonce().$created.$encodedPassword));
-
-        $tokenAuthenticated = $this->getApplication()->getSecurityContext()->getAuthenticationManager()
-            ->authenticate($token)
-        ;
+            ->getAuthenticationManager()
+            ->authenticate($token);
 
         if (!$tokenAuthenticated->getUser()->getApiKeyEnabled()) {
             throw new DisabledException('API access forbidden');
@@ -74,7 +76,7 @@ class SecurityController extends AbstractRestController
         $this->getApplication()->getSecurityContext()->setToken($tokenAuthenticated);
 
         return $this->createJsonResponse(null, 201, array(
-            'X-API-KEY'       => $tokenAuthenticated->getUser()->getApiKeyPublic(),
+            'X-API-KEY' => $tokenAuthenticated->getUser()->getApiKeyPublic(),
             'X-API-SIGNATURE' => $tokenAuthenticated->getNonce(),
         ));
     }
@@ -82,7 +84,7 @@ class SecurityController extends AbstractRestController
     /**
      * @Rest\Security(expression="is_fully_authenticated()")
      */
-    public function deleteSessionAction(Request $request)
+    public function deleteSessionAction(Request $request): Response
     {
         if (null === $request->getSession()) {
             throw new NotFoundHttpException('Session doesn\'t exist');
